ef33fd2a211731e8350c2fc751fb458f4f40a6bc
NELL RCS <nellrcs@gmail.com>
2015-11-16 22:10:21 +0000

    App

---
+++ www/js/app.js
@@ -0,0 +1,51 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider,$urlRouterProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: 'home.html',
+    controller: 'homeController'
+  })
+  .state('home', {
+    url: '/home',
+    templateUrl: 'home.html',
+    controller: 'homeController'
+  })
+  .state('lista', {
+    url: '/lista',
+    templateUrl: 'lista.html',
+    controller: 'homeController'
+  })
+  .state('imagem', {
+    url: '/imagem',
+    templateUrl: 'imagem.html',
+    controller: 'homeController'
+  });
+  
+  $urlRouterProvider.otherwise('/');
+
+})
+.controller('homeController', function($scope, $stateParams, $ionicHistory) {
+  $scope.voltar = function(){
+    $ionicHistory.goBack();
+  }    
+});
